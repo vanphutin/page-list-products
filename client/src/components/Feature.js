d28d6e1d4b3d3e5330b81fab5eecd96d7dc58d58
vanphutin2004 <vanphutin.contact@gmail.com>
2024-07-09 15:18:10 +0700

    feature sort

--- client/src/components/Feature.js
+++ client/src/components/Feature.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Feature = () => {
+const Feature = ({ handleSortAsc, handleSortDesc }) => {
   return (
     <div style={{ with: "100%" }}>
       <div className="sort">
-        Sort by price <button>UP</button> <button>DOWM</button>
+        Sort by price <button onClick={handleSortAsc}>ASC</button>{" "}
+        <button onClick={handleSortDesc}>DESC</button>
       </div>
     </div>
   );
