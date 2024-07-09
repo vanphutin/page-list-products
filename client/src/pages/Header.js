c61d38e6f979547a3493303577db56328236fb1d
vanphutin2004 <vanphutin.contact@gmail.com>
2024-07-09 15:19:39 +0700

    header page

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,52 +1,37 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a
-        href="/"
-        class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
-      >
-        <h4>LIST PRODUCTS</h4>
-      </a>
+    <>
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a
+          href="/"
+          className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+        >
+          <h4>LIST PRODUCTS</h4>
+        </a>
 
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li>
-          <a href="#" class="nav-link px-2 link-secondary">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link px-2 link-dark">
-            Features
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link px-2 link-dark">
-            Pricing
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link px-2 link-dark">
-            FAQs
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link px-2 link-dark">
-            About
-          </a>
-        </li>
-      </ul>
+        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin</Link>
+          </li>
+        </ul>
 
-      <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">
-          Login
-        </button>
-        <button type="button" class="btn btn-primary">
-          Sign-up
-        </button>
-      </div>
-    </header>
+        <div className="col-md-3 text-end">
+          <button type="button" className="btn btn-outline-primary me-2">
+            Login
+          </button>
+          <button type="button" className="btn btn-primary">
+            Sign-up
+          </button>
+        </div>
+      </header>
+      <Outlet />
+    </>
   );
 };
 
